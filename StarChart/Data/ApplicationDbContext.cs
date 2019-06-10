cce0a6415761246d436d3d35d7f28b9bdcf3b373
zbaker <zcbaker@ymail.com>
2019-06-09 17:23:21 -0700

    Creat CelestialObject Model
    Created the celestial object model and added it to the application db context.

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObjects> CelestialObjects;
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
---
+++ StarChart/Models/CelestialObjects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObjects
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObject { get; set; }
+        [NotMapped]
+        public List<CelestialObjects> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
