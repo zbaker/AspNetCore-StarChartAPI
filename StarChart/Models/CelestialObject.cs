140ff09733dda74d125670371c428de23abf2237
zbaker <zcbaker@ymail.com>
2019-06-09 17:29:07 -0700

    Added Get+Set for CelestialObjects
    Added get and set to CelestialObjects and corrected property misspell to now be OrbitedObjectId.

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,7 +9,7 @@ namespace StarChart.Models
     {  public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public int? OrbitedObject { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
